4d191cabaa1f74d925acf3419f67ab44d78b49bd
avaschmidt <ava.schmidt25@d155.org>
2025-01-30 07:35:10 -0600

    Initial Commit

---
+++ FirebaseNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseNotes
+//
+//  Created by AVA SCHMIDT on 1/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirebaseNotesTests/FirebaseNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseNotesTests.swift
+//  FirebaseNotesTests
+//
+//  Created by AVA SCHMIDT on 1/30/25.
+//
+
+import Testing
+@testable import FirebaseNotes
+
+struct FirebaseNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
